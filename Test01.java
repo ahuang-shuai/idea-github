ae943fd18f1fc6592690434f05f4c86083041adb
ahuang <ahuang@163.com>
2023-05-10 21:45:31 +0800

    hot-fix-third commit

--- Test01.java
+++ Test01.java
@@ -3,5 +3,6 @@ public class Test01 {
         System.out.println("IDEA中集成Git测试程序");
         System.out.println("测试版本切换");
         System.out.println("hot-fix分支");
+        System.out.println("master测试合并冲突");
     }
 }
