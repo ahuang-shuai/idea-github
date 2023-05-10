630caa45ab28613f5a092d1e8a96463c41745601
ahuang <ahuang@163.com>
2023-05-10 21:38:36 +0800

    hot-fix-first commit

--- Test01.java
+++ Test01.java
@@ -2,5 +2,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("IDEA中集成Git测试程序");
         System.out.println("测试版本切换");
+        System.out.println("hot-fix分支");
     }
 }
